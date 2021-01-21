09390a01331329d993a6f5b5705a4faac133ba49
GregGaH <greg.gaul.work@gmail.com>
2021-01-21 07:30:00 +0100

    initial commit, repo remade

---
+++ src/main/java/com/webshop/app/FallzaShopApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallzaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallzaShopApplication.class, args);
+    }
+
+}
